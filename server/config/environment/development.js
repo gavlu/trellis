15fbcdeff00a9aa785cf66d3c67e7a21e197425b
gavlu <gavinlue@gmail.com>
2014-11-24 12:40:07 -0500

    Scaffolded the app using angular-fullstack generator.

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/trellis-dev'
+  },
+
+  seedDB: true
+};
