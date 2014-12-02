f2aab7662ec9d9f4fad4dc6f8f258dcaa25c64fc
JFKelly68 <mrjamesfkelly@gmail.com>
2014-12-02 10:40:35 -0500

    Got angularstrap working. The docs are dumb

--- client/app/newPlant/newPlant.controller.js
+++ client/app/newPlant/newPlant.controller.js
@@ -2,5 +2,12 @@
 
 angular.module('trellisApp')
   .controller('NewplantCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.selectedIcon = "Gear";
+	$scope.selectedIcons = ['Gear','Globe','Heart','Camera'];
+	$scope.icons = [
+	    {value: 'Gear', label: '<i class="fa fa-gear"></i> Gear'},
+	    {value: 'Globe', label: '<i class="fa fa-globe"></i> Globe'},
+	    {value: 'Heart', label: '<i class="fa fa-heart"></i> Heart'},
+	    {value: 'Camera', label: '<i class="fa fa-camera"></i> Camera'}
+	];
   });
